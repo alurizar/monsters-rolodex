2d77734160f1eaaaca184efa1f0b79c8940f6bd8
Alejandro Urizar <aurizar@xpediantsolutions.com>
2020-03-10 11:42:38 -0600

    Updates

---
+++ precache-manifest.680d4873b26e0283de029f896d6916d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4875c0429f724948247f6a45326572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd10e739b1564c434eac",
+    "url": "/static/css/main.84aaf4ad.chunk.css"
+  },
+  {
+    "revision": "4c68d3c9df0094ce6f6c",
+    "url": "/static/js/2.8c99312d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8c99312d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd10e739b1564c434eac",
+    "url": "/static/js/main.f4e205a3.chunk.js"
+  },
+  {
+    "revision": "a53c56bcba93a6115910",
+    "url": "/static/js/runtime-main.4595a359.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
