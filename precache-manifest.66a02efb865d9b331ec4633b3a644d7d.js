948f0c2596801560620de30b83d9629c1f59f18e
Alejandro Urizar <aurizar@xpediantsolutions.com>
2020-03-10 12:01:52 -0600

    Updates

---
+++ precache-manifest.66a02efb865d9b331ec4633b3a644d7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec9ec5d97528f7af27b592593323fbe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89ccf35954fbc00d7f5c",
+    "url": "/monsters-rolodex/static/css/main.84aaf4ad.chunk.css"
+  },
+  {
+    "revision": "4c68d3c9df0094ce6f6c",
+    "url": "/monsters-rolodex/static/js/2.8c99312d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.8c99312d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ccf35954fbc00d7f5c",
+    "url": "/monsters-rolodex/static/js/main.b8a30fec.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.680d4873b26e0283de029f896d6916d8.js"
+  "/monsters-rolodex/precache-manifest.66a02efb865d9b331ec4633b3a644d7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
